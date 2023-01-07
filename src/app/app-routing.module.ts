39926ceb74634cc291046acbd4215df50d9338ea
Zerouali Amine <amirez4444@gmail.com>
2023-01-07 20:08:00 +0100

    working version 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './common/auth.guard';
 
 const routes: Routes = [
   {
@@ -10,6 +11,13 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./authentification/Authentification.module').then(
+        (m) => m.AuthentificationModule
+      ),
   },
 ];
 
